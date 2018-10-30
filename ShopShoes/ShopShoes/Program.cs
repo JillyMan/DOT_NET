7ca8306edec799a06a861ff0b8cce0c8b7c9b31b
Artem <artyom543211@gmail.com>
2018-10-30 18:04:37 +0300

    init commit

---
+++ ShopShoes/ShopShoes/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ShopShoes
+{
+	public class Program
+	{
+		public static void Main(string[] args)
+		{
+			Console.BackgroundColor = ConsoleColor.DarkBlue;
+			Console.WriteLine("Hello World");
+		}
+	}
+}
